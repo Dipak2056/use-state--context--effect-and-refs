b2c3f0de9b2cd78296f1743cf9372344b25dbf2f
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-27 11:44:19 +1000

    i implemented it by myself

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LoginContext } from "../Helper/Context";
 
 const Profile = () => {
+  const { loggedin, setLoggedin } = useContext(LoginContext);
   return (
     <div className="container">
       <h1>profile</h1>
+      <button onClick={() => setLoggedin(!loggedin)}>click to toggle</button>
+      {loggedin ? <h1>you are logged in</h1> : <h1>you are not logged in</h1>}
     </div>
   );
 };
