c02acc69434c6f1c4635742eefb35cb6be9e013e
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-28 23:07:32 +1000

    basic form is done

--- src/components/Form/FormComponent.jsx
+++ src/components/Form/FormComponent.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
-const inititalState = { firstname: "", lastname: "", email: "" };
+const inititalState = {
+  firstname: "",
+  lastname: "",
+  email: "",
+  comment: "",
+  isFriendly: true,
+};
 export const FormComponent = () => {
   const [formData, setformData] = useState(inititalState);
 
   const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
     setformData((prevFormData) => {
       return {
         ...prevFormData,
-        [e.target.name]: e.target.value,
+        [name]: type === "checkbox" ? checked : value,
       };
     });
+    console.log(formData);
   };
-  console.log(formData);
   return (
     <div>
       <input
@@ -19,12 +26,14 @@ export const FormComponent = () => {
         placeholder="first-name"
         name="firstname"
         onChange={handleChange}
+        value={formData.firstname}
       />
       <input
         type="text"
         placeholder="first-name"
         name="lastname"
         onChange={handleChange}
+        value={formData.lastname}
       />
       <input
         type="email"
@@ -32,6 +41,21 @@ export const FormComponent = () => {
         placeholder="email@"
         name="email"
         onChange={handleChange}
+        value={formData.email}
+      />
+      <textarea
+        placeholder="textarea"
+        value={formData.textarea}
+        onChange={handleChange}
+        name="comment"
+      />
+      <label htmlFor="isfriendly">Are you friendly</label>
+      <input
+        onChange={handleChange}
+        type="checkbox"
+        id="isfriendly"
+        name="isFriendly"
+        checked={formData.isFriendly}
       />
     </div>
   );
